749d432622202dad05f8f269697bc545792d6b17
nextz0 <ratchanonchantree11@gmail.com>
2026-01-19 17:52:43 +0700

    refactor: change key database and ignore

--- backend/go.mod
+++ backend/go.mod
@@ -5,5 +5,6 @@ go 1.25.3
 require (
 	github.com/go-viper/mapstructure/v2 v2.2.1 // indirect
 	github.com/google/go-querystring v1.1.0 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/nedpals/supabase-go v0.5.0 // indirect
 )
--- backend/main.go
+++ backend/main.go
@@ -3,13 +3,13 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"log" 
 	"net/http"
+	"os"
 
+	"github.com/joho/godotenv"
 	"github.com/nedpals/supabase-go"
 )
-
-const supabaseUrl = "https://ixreecjqnzroibwvuzxx.supabase.co"
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml4cmVlY2pxbnpyb2lid3Z1enh4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU0MzQ3NjAsImV4cCI6MjA4MTAxMDc2MH0.8_ULIKMHEOoOdNi_pNcHcg_rYEIqaKofAtuUA61lSJg"
 
 type TimerLog struct {
 	Duration int    `json:"duration"`
@@ -17,7 +17,7 @@ type TimerLog struct {
 }
 
 func saveSessionHandler(w http.ResponseWriter, r *http.Request) {
-	// ตั้งค่า CORS (สำคัญมาก ไม่งั้น Frontend จะยิงไม่เข้า)
+	// ตั้งค่า CORS
 	w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
 	w.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS")
 	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
@@ -27,18 +27,30 @@ func saveSessionHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	if r.Method == "POST" {
-		var log TimerLog
+		var logData TimerLog
+		
 		// รับข้อมูล JSON จาก Frontend
-		err := json.NewDecoder(r.Body).Decode(&log)
+		err := json.NewDecoder(r.Body).Decode(&logData)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusBadRequest)
 			return
 		}
 
+		supabaseUrl := os.Getenv("SUPABASE_URL")
+		supabaseKey := os.Getenv("SUPABASE_KEY")
+
+		if supabaseUrl == "" || supabaseKey == "" {
+			fmt.Println("Error: SUPABASE_URL or SUPABASE_KEY not found in .env")
+			http.Error(w, "Server configuration error", http.StatusInternalServerError)
+			return
+		}
+
+		// สร้าง Client
 		client := supabase.CreateClient(supabaseUrl, supabaseKey)
 
+		// บันทึกลง Database
 		var results []TimerLog
-		err = client.DB.From("sessions").Insert(log).Execute(&results)
+		err = client.DB.From("sessions").Insert(logData).Execute(&results)
 
 		if err != nil {
 			fmt.Println("Error inserting to Supabase:", err)
@@ -55,6 +67,11 @@ func saveSessionHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("Warning: Error loading .env file (อาจจะรันบน Server หรือหาไฟล์ไม่เจอ)")
+	}
+
 	http.HandleFunc("/api/save-session", saveSessionHandler)
 	fmt.Println("Server is running on port 8080...")
 	http.ListenAndServe(":8080", nil)
